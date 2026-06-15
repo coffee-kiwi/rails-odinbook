f1c30578fc3387b38ca11c23ae125fc31f0cb532
meltingseaweed <conor.seo@gmail.com>
2026-06-15 13:45:48 +0900

    Fix: Alter code to fit with commented rails example

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   #   port: 587,
   #   authentication: :plain
   # }
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
   user_name: "apikey", # This is the string literal 'apikey', NOT the ID of your API key
   password: ENV["SENDGRID_API_ODIN"], # This is the secret sendgrid API key which was issued during API key creation
   domain: "example.com",
