c96903281f84d86d523f0a20ce89f16f217969fd
meltingseaweed <conor.seo@gmail.com>
2026-06-15 09:28:06 +0900

    Style: Add Title to posts, to make it easier to identify different postson the user's show page and also index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_13_063821) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_15_002339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_13_063821) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
+    t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
