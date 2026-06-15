7115edf95970f988cece924b3c70818f0e326555
meltingseaweed <conor.seo@gmail.com>
2026-06-15 10:57:23 +0900

    Fix: Permit Username to strong paramers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
   stale_when_importmap_changes
   # Always check authentication before anything
   before_action :authenticate_user!
+
+  # Add username to strong parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
+  end
 end
