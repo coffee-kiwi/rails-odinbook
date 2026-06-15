aa9c483688e7bcbb76fbd9015ed82b1d48c1cc04
meltingseaweed <conor.seo@gmail.com>
2026-06-15 13:52:31 +0900

    Fix: Update domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   user_name: "apikey", # This is the string literal 'apikey', NOT the ID of your API key
   password: ENV["SENDGRID_API_ODIN"], # This is the secret sendgrid API key which was issued during API key creation
-  domain: "example.com",
+  domain: "https://odinbook-jiuf.onrender.com",
   address: "smtp.sendgrid.net",
   port: 587,
   authentication: :plain,
