9598e1db9d66220f50d922770621b4319132dc67
meltingseaweed <conor.seo@gmail.com>
2026-06-16 12:33:34 +0900

    Fix: Render deploy taking very long, perhaps due to the requests in render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,7 @@ set -o errexit
 bundle install
 bin/rails assets:precompile
 bin/rails assets:clean
-
+# bundle add solid_queue
+# bin/rails solid_queue:install
+# bundle exec rake solid_queue:start
 bin/rails db:prepare
-bundle exec rake solid_queue:start
