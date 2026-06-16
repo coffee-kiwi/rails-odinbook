30bdbcebf08ac055e09aaddd10200699fddd8596
meltingseaweed <conor.seo@gmail.com>
2026-06-16 14:11:14 +0900

    Silence puma logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  #
+  # Silence Puma logs
+  config.solid_queue.silence_polling = true
 end
