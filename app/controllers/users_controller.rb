57e793007f36e25e9a730fc9ed67e752e99b66d5
meltingseaweed <conor.seo@gmail.com>
2026-06-20 09:20:23 +0900

    Feat: Try to resize image before uploading in order to reduce unnecessary big load times

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action lambda {
+    resize_before_save(avatar_params, 300, 300)
+  }, only: [ :update ]
   def index
     @users = User.all
     @my_pending_requests = FollowerRequest.where("user_id = ? AND status = ?", current_user.id, 1)
@@ -34,5 +37,24 @@ class UsersController < ApplicationController
 
   def update_params
     params.expect(user: [ :username, :bio, :avatar ])
+  end
+
+  def avatar_params
+    params.expect(user: [ :avatar ])
+  end
+
+  def resize_before_save(image_param, width, height)
+    return unless image_param
+
+    begin
+      ImageProcessing::MiniMagick
+        .source(image_param)
+        .resize_to_fit(width, height)
+        .call(destination: image_param.tempfile.path)
+    rescue StandardError => _e
+      # Do nothing. If this is catching, it probably means the
+      # file type is incorrect, which can be caught later by
+      # model validations.
+    end
   end
 end
