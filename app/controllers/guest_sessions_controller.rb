6e4af5920ff3575934e582a857c8a093b890b1cc
meltingseaweed <conor.seo@gmail.com>
2026-09-09 16:24:13 +0900

    Fix: Only do one query

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,7 +1,7 @@
 class GuestSessionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :create ]
   def create
-    guest = User.find_by!(email: "guest@example.com")
+    # guest = User.find_by!(email: "guest@example.com")
     guest = User.find_by!("email = ?", "guest@example.com")
     sign_in(guest)
     redirect_to root_path
