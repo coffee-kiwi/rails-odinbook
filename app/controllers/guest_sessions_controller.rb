0797a107b61002137487a3d389395fb4a588cbab
meltingseaweed <conor.seo@gmail.com>
2026-09-09 16:28:45 +0900

    Fix: Try using simple command

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,8 +1,8 @@
 class GuestSessionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :create ]
   def create
-    # guest = User.find_by!(email: "guest@example.com")
-    guest = User.find_by!("email = ?", "guest@example.com")
+    guest = User.find_by!(email: "guest@example.com")
+    # guest = User.find_by!("email = ?", "guest@example.com")
     sign_in(guest)
     redirect_to root_path
   end
